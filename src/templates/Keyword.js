3c4315471f68af7cd9860f52a08e14ca1b415321
DaciteRocks <Conno3333@gmail.com>
2020-10-29 14:41:37 -0700

    fixed categories issue with ItemThumbnail

--- src/templates/Keyword.js
+++ src/templates/Keyword.js
@@ -25,11 +25,11 @@ const Keys = ({ pageContext, data }) => {
         <ThumbnailsWrapper>
             {edges.map(({ node }) => (
                 <div key={node.id}>
-                    <ItemThumbnail key={node.fields.slug} link={node.fields.slug} heading={node.frontmatter.title} price={node.frontmatter.price} imageThumb={node.frontmatter.image.childImageSharp.fluid} active={node.frontmatter.active}/>
+                    <ItemThumbnail key={node.fields.slug} options={node.frontmatter.options} link={node.fields.slug} heading={node.frontmatter.title} price={node.frontmatter.price} imageThumb={node.frontmatter.image.childImageSharp.fluid} active={node.frontmatter.active}/>
                     {node.frontmatter.active == true &&
                         <div>
                             <div>
-                                <PaypalComponent  options = { node.frontmatter.options} title = { node.frontmatter.title} paypalInput ={node.frontmatter.price} />
+                                <PaypalComponent  options = { node.frontmatter.options} title = { node.frontmatter.title} price ={node.frontmatter.price} />
                             </div>
                         </div>
                     }
