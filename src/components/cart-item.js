872b2f5366cbde08435c9fd22935218c203362bc
shuynh3 <47999699+shuynh3@users.noreply.github.com>
2020-11-12 23:56:05 -0700

    Added styling to Checkout Page

--- src/components/cart-item.js
+++ src/components/cart-item.js
@@ -1,5 +1,94 @@
 import React from 'react'
 import { useCart } from '../../plugins/react-cart/react-cart'
+import styled from "styled-components"
+
+const CartItemDiv = styled.div`
+    display: inline-block;
+    width: 315px;
+    height: 400px;
+    margin: 10px;
+    background-color: white;
+    box-shadow: 0px 0px 5px rgba(0,0,0,0.2);
+`
+
+const CartItemInfo = styled.div`
+    width: 315px;
+    height: 150px;
+    fill: #ffffff;
+    background-color: white;
+    box-shadow: 0px 0px 5px rgba(0,0,0,0.2);
+`
+
+const ImageDiv = styled.div`
+    width: 315px;
+    height: 250px;
+    background-color: lightgray;
+`
+
+const InfoContainer = styled.div`
+  margin-left: 15px;
+  margin-right: 15px;
+`
+
+const TitleStyle = styled.span`
+  font-family: Roboto;
+  font-size: 25px;
+  color: #000000;
+  text-decoration: none solid rgb(0, 0, 0);
+
+  margin-bottom: 15px;
+
+  display: inline-block;
+  vertical-align: bottom;
+
+  width: 270px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
+
+const TextStyle = styled.span`
+  font-family: Roboto;
+  font-size: 20px;
+  color: #000000;
+  text-decoration: none solid rgb(0, 0, 0);
+  display: inline-block;
+`
+
+const TextStyleQuantity = styled.p`
+  font-family: Roboto;
+  font-size: 20px;
+  color: #000000;
+  text-decoration: none solid rgb(0, 0, 0);
+  margin-left: 15px;
+  display: inline;
+`
+
+const TextStyleOption = styled.p`
+  font-family: Roboto;
+  font-size: 20px;
+  color: #000000;
+  text-decoration: none solid rgb(0, 0, 0);
+  margin-left: 30px;
+  display: inline-block;
+  vertical-align: bottom;
+
+  height: 23px;
+  width: 180px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
+
+const IncrementStyle = styled.span`
+  margin-left: 10px;
+`
+
+const RemoveButton = styled.div`
+  margin-top: 15px;
+  margin-left: 186px;
+`
+
 //import { useCart } from 'react-use-cart'
 function CartItem({ id, name, description, quantity}) {
   const { updateItemQuantity, removeItem } = useCart()
@@ -9,43 +98,60 @@ function CartItem({ id, name, description, quantity}) {
   const remove = () => removeItem(id)
 
   return (
-    <div key={id} className="cart-list-item">
-      <div className="cart-list-item--name">
-        {name}
-        <div>
-        {description}
-          </div> 
+    <CartItemDiv key={id} className="cart-list-item">
+      <ImageDiv>
         
-       
-      </div>
+      </ImageDiv>
 
-      <div className="cart-list-item--quantity">
-        <div className="cart-item-quantity--wrapper">
-          <button
-            onClick={decrement}
-            className="cart-list-item-button cart-list-item-button--decrement"
-          >
-            -
-          </button>
+      <CartItemInfo>
 
-          <span className="cart-item-quantity--amount">{quantity}</span>
+        <InfoContainer>
+          <br/>
+          <div className="cart-list-item--name">
+            <TitleStyle>{name}</TitleStyle>
+          </div>
 
-          <button
-            onClick={increment}
-            className="cart-list-item-button cart-list-item-button--increment"
-          >
-            +
-          </button>
-        </div>
+          <div className="cart-list-item--quantity">
 
-        <button
-          onClick={remove}
-          className="cart-list-item-button cart-list-item-button--remove-item"
-        >
-          Remove item
-        </button>
-      </div>
-    </div>
+            <div className="cart-item-quantity--wrapper">
+              <TextStyle>Quantity: <TextStyleQuantity className="cart-item-quantity--amount">{quantity}</TextStyleQuantity></TextStyle>
+
+              <IncrementStyle>
+                <button
+                  onClick={decrement}
+                  className="cart-list-item-button cart-list-item-button--decrement"
+                >
+                  -
+                </button>
+
+                <button
+                  onClick={increment}
+                  className="cart-list-item-button cart-list-item-button--increment"
+                >
+                  +
+                </button>
+              </IncrementStyle>
+            </div>
+
+            <TextStyle>Option: <TextStyleOption>{description}</TextStyleOption></TextStyle> 
+
+            <br/>
+
+            <RemoveButton>
+              <button
+                onClick={remove}
+                className="cart-list-item-button cart-list-item-button--remove-item"
+              >
+                Remove item
+              </button>
+            </RemoveButton>
+            
+
+          </div>
+        </InfoContainer>
+
+      </CartItemInfo>
+    </CartItemDiv>
   )
 }
 
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,12 +1,63 @@
 import React from 'react'
 
-import {graphql} from "gatsby"
 import Layout from "../components/layout"
 import  Cart  from '../components/cart'
 import PaypalButton from "../components/paypalButton"
+import styled from "styled-components"
 
 import { useCart } from '../../plugins/react-cart/react-cart'
 
+const CartArea = styled.div`
+    max-width: 800px;
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    flex-wrap: wrap;
+    padding: 10px;
+    margin: auto;
+`
+
+const CartTitle = styled.h1`
+    font-family: Roboto;
+    font-size: 28px;
+    color: #222222;
+    text-decoration: none solid rgb(34, 34, 34);
+    margin-top: 39px;
+    margin-bottom: 40px;
+    margin-left: 30%;
+`
+const CartText = styled.p`
+    width: 150px;
+    height: 24px;
+    font-family: Roboto;
+    font-size: 20px;
+    color: #000000;
+    text-decoration: none solid rgb(0, 0, 0);
+    margin-top: 40px;
+    margin-bottom: 0px;
+    margin-left: 30%;
+`
+
+const OrderButton = styled.div`
+    width: 150px;
+    height: 24px;
+    font-family: Roboto;
+    font-size: 20px;
+    color: #000000;
+    text-decoration: none solid rgb(0, 0, 0);
+    margin-top: 24px;
+    margin-left: 30%;
+    margin-bottom: 110px;
+`
+
+const CartPrice = styled.h1`
+    font-family: Roboto;
+    font-size: 28px;
+    color: #000000;
+    text-decoration: none solid rgb(0, 0, 0);
+    margin-top: 3px;
+    margin-left: 30%;
+`
 
 
 function Returntotal(){
@@ -19,9 +70,27 @@ function Returntotal(){
 
 const CheckoutPage = () => (
   <Layout>
-    <h1>Cart</h1>
-    <Cart />
-    Total is : $<Returntotal></Returntotal>
+    <CartTitle>
+      MY CART
+    </CartTitle>
+
+    <CartArea>
+        <Cart/>
+    </CartArea>
+
+    <CartText>
+      TOTAL
+    </CartText>
+
+    <CartPrice>
+      $<Returntotal/> USD
+    </CartPrice>
+
+    <OrderButton>
+      <PaypalButton price={0} title = {0} option ={0}  amount={0} />
+    </OrderButton>
+    
+    
     
   </Layout>
 )
