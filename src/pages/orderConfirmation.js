241ae6ba1e4cbcbed9080eb01e3998e229dea1b0
strobehello <61479838+strobehello@users.noreply.github.com>
2020-11-14 19:52:48 -0700

    Update orderConfirmation.js

--- src/pages/orderConfirmation.js
+++ src/pages/orderConfirmation.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import Layout from "../components/layout"
+
 const OrderConfirmationPage = () => (
   <Layout>
-    <header>Thank you! </header>
-    
-    <paragraph>
-    Youre order has been placed.
-    Please check your PayPal account for transaction details.     
-    
-    </paragraph>
+    <h1 style={{color: 'black', fontSize: 60, textAlign: 'center'}}>
+      <b>THANK YOU.</b>
+    </h1>
 
+    <p style={{color: 'black', fontSize: 24, textAlign: 'center'}}>
+      Your order has been placed. <br></br>
+      Please check your PayPal account for transaction details.   
+    </p>
   </Layout>
 )
 
