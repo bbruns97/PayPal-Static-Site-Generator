f3e43860bd62f8700ebf292b27aaa5e9cb6dc7cc
Brian Bruns <bbruns97@gmail.com>
2020-11-24 00:24:24 -0700

    Updates

--- root-wrapper.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { CartProvider } from "./plugins/react-cart/react-cart"
-
-export const wrapPageElement = ({ element }) => (
-  <>
-    <CartProvider>{element}</CartProvider>
-  </>
-)
